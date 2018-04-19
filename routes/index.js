cf76be34d45e6f952c4a8cbef9eae4fb6a94ad28
Morris Tai <leaper151@outlook.com>
2018-04-19 14:25:07 +0800

    jock modified

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '資料庫報告' });
+  res.render('index', { title: '謝竣翔' });
 });
 
 module.exports = router;
